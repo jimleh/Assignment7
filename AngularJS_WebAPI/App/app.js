85b9cb3952b68090c0a65c5a89f1fd0b4fca25b3
jimleh <jimmy.lehtola@gmail.com>
2017-11-17 09:29:44 +0100

    Small update to app.js and Index.cshtml

--- AngularJS_WebAPI/App/app.js
+++ AngularJS_WebAPI/App/app.js
@@ -1,14 +1,14 @@
 ﻿(function () {
     var app = angular.module("myApp", []);
 
-    var myController = function ($scope, $http) {
+    var myController = function ($scope, $http, $compile) {
 
         var GetData = function () {
             $http.get("../api/person/get")
                     .then(function (response) {
                         $scope.persons = response.data;
                         GetCities();
-            });      
+                    });
         };
 
         var GetCities = function () {
@@ -25,12 +25,10 @@
         $scope.sorter = "+ID";
 
         $scope.sortData = function (sorter) {
-            if($scope.sorter.includes("+"))
-            {
+            if ($scope.sorter.includes("+")) {
                 $scope.sorter = "-" + sorter;
             }
-            else
-            {
+            else {
                 $scope.sorter = "+" + sorter;
             }
 
@@ -42,45 +40,66 @@
             console.log("Sorting by: " + $scope.sorter);
         };
 
-        $scope.sendData = function () {
-            for (var i = 0; i < $scope.persons.length; i++)
-            {
-                if(!$scope.Person.ID || $scope.persons[i].ID >= $scope.Person.ID)
-                {
-                    $scope.Person.ID = $scope.persons[i].ID + 1;
-                }
-            }
-            $http.post("../api/person/post", $scope.Person)
-                .then(function () {
-                    GetData();
-
-                    console.log($scope.Person.Name + " was added successfully!");
-                });
-        };
-
         $scope.deleteData = function (id) {
             $http.delete("../api/person/delete/" + id)
-                .then(function () {
+                .then(function (response) {
                     GetData();
-                    console.log("Delete successful!");
+                    console.log(response.data);
                 })
         };
 
         $scope.startEdit = function (person) {
-            $scope.edit = true;
-            $scope.Person = person;
+
+            var input = document.getElementById("input");
+
+            if (!$scope.edit) {
+
+                $scope.edit = true;
+                $scope.Person = person;
+
+                input.classList.add("alert-success");
+                document.getElementById("submitBtn").value = "Edit!";
+                var div = angular.element(input.children.item(0));
+                div.append(
+                        $compile('<input id="abortBtn" ng-click="startEdit()" class="btn btn-warning" type="button" value="Abort!"/>')($scope)
+                 );
+            }
+            else {
+                $scope.edit = false;
+                $scope.Person = {};
+                GetCities();
+                document.getElementById("submitBtn").value = "Add!";
+                var btn = document.getElementById("abortBtn");
+                btn.parentNode.removeChild(btn);
+                input.classList.remove("alert-success");
+            }
+
         };
 
-        $scope.editData = function () {
-            $http.put("../api/person/put/" + $scope.Person.ID, $scope.Person)
-                .then(function () {
-                    console.log($scope.Person.Name + " was edited successfully!");
-                    $scope.edit = false;
-                    $scope.Person = {};
-                    GetCities();
+        $scope.handleInput = function () {
+            if ($scope.edit) {
+                $http.put("../api/person/put/" + $scope.Person.ID, $scope.Person)
+                .then(function (response) {
+                    console.log(response.data);
+                    $scope.startEdit();
                 });
+            }
+            else {
+                var length = $scope.persons.length;
+                for (var i = 0; i < length; i++) {
+                    if (!$scope.Person.ID || $scope.persons[i].ID >= $scope.Person.ID) {
+                        $scope.Person.ID = $scope.persons[i].ID + 1;
+                    }
+                };
+                $http.post("../api/person/post", $scope.Person)
+                    .then(function () {
+                        GetData();
+
+                        console.log($scope.Person.Name + " was added successfully!");
+                    });
+            }
         };
     };
 
-    app.controller("myController", ["$scope", "$http", myController]);
+    app.controller("myController", ["$scope", "$http", "$compile", myController]);
 }());
